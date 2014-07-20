3db4f616b69f1fcd4c4ef433103b07c7cc343cdb
Jason <jyc8@live.com>
2014-07-20 12:49:26 -0700

    Fading Script QoL Fixes

--- Assets/Jason/Script/MusicObject.cs
+++ Assets/Jason/Script/MusicObject.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class MusicObject : MonoBehaviour {
+	bool fading = false;
 
 	// Use this for initialization
 	void Start () {
@@ -13,19 +14,27 @@ public class MusicObject : MonoBehaviour {
 
 	public void Activate(){
 		Debug.Log("Music Object Activated");
+		fading = false;
 		audio.Play();
+		audio.volume = 1;
 	}
 
 	public void Deactivate(){
 		Debug.Log("Music Object Deactivated");
+		fading = true;
 		StartCoroutine(FadeMusicOut());
 	}
 
 	IEnumerator FadeMusicOut(){
-		for (float i = 9; i > 0; i--){
+		for (float i = 5; i > 0; i--){
 			audio.volume = audio.volume - audio.volume * 0.3f;
 			yield return new WaitForSeconds (0.3f);
+			if (!fading){
+				break;
+			}
 		}
-		audio.Stop();
+		if (fading){
+			audio.Stop();
+		}
 	}
 }
