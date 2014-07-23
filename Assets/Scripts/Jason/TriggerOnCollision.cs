9e31eb77af70cc80875539f2a6375a1da0ec8fca
Jason <jyc8@live.com>
2014-07-23 14:56:59 -0700

    Added Fall Scripts

---
+++ Assets/Scripts/Jason/TriggerOnCollision.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerOnCollision : MonoBehaviour {
+	public bool loadLevel = false;
+	public string level = "Scene Name";
+	public bool warp = false;
+	public float warpDistance;
+	public float warpHeight;
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log("Something has entered this zone.");
+		if (loadLevel){
+			Application.LoadLevel(level);
+		}
+		if (warp){
+			Debug.Log("Warping.");
+			other.gameObject.transform.localPosition = new Vector3( other.gameObject.transform.position.x + warpDistance, warpHeight, 1f );
+		}
+	} 
+}
