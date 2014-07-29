8ef95b443a1e604a6f7e88fdeda910d2668acaec
Jason <jyc8@live.com>
2014-07-29 14:34:35 -0700

    Intro

---
+++ Assets/Scripts/Jason/EnableOnStart.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableOnStart : MonoBehaviour {
+	public GameObject[] obj;
+	
+	void Start(){
+		for (int i = 0; i < obj.Length; i++)
+			obj[i].SetActive(true);
+	}
+}
