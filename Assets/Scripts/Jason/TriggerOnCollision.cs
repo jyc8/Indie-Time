5565f6f7a6c9cea8d1c602f74c3eeba95d38c6e4
Jason <jyc8@live.com>
2014-07-29 14:50:30 -0700

    Audio Update

--- Assets/Scripts/Jason/TriggerOnCollision.cs
+++ Assets/Scripts/Jason/TriggerOnCollision.cs
@@ -2,20 +2,33 @@
 using System.Collections;
 
 public class TriggerOnCollision : MonoBehaviour {
+	private bool triggered = false;
+	public bool triggerOnce = false;
+	public bool audioTrigger = false;
 	public bool loadLevel = false;
 	public string level = "Scene Name";
 	public bool warp = false;
 	public float warpDistance;
 	public float warpHeight;
 
+
 	void OnTriggerEnter2D(Collider2D other){
 		Debug.Log("Something has entered this zone.");
-		if (loadLevel){
-			Application.LoadLevel(level);
+		if (triggerOnce == true && triggered == true){
+			
+		} else{
+			if (loadLevel){
+				Application.LoadLevel(level);
+			}
+			if (warp){
+				Debug.Log("Warping.");
+				other.gameObject.transform.localPosition = new Vector3( other.gameObject.transform.position.x + warpDistance, warpHeight, 1f );
+			}
+			if (audioTrigger){
+				audio.Play();
+			}
+			triggered = true;
 		}
-		if (warp){
-			Debug.Log("Warping.");
-			other.gameObject.transform.localPosition = new Vector3( other.gameObject.transform.position.x + warpDistance, warpHeight, 1f );
-		}
+
 	} 
 }
