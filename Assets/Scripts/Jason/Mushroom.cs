357d3fd575786b417bbd4e6d7f7e4f22f54ae455
Jason <jyc8@live.com>
2014-07-29 20:32:46 -0700

    Mushroom

---
+++ Assets/Scripts/Jason/Mushroom.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mushroom : MonoBehaviour {
+	public float bounceForce = 10;
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Player")
+		{	
+			Debug.Log ("Bounce");
+			other.rigidbody2D.AddForce(bounceForce * other.transform.up, ForceMode2D.Impulse);
+		}
+	}
+}
