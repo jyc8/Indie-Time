04431d9a13689f6abc66341a764d61e2c06ef50b
Jason <jyc8@live.com>
2014-07-30 00:14:25 -0700

    Planet

---
+++ Assets/Scripts/Jason/PlanetSpin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetSpin : MonoBehaviour {
+	public GameObject planet;
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
