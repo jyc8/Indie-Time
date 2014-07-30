2735af0ce4499fff51d5280c9704b17b0bb085a9
Jason <jyc8@live.com>
2014-07-30 14:36:18 -0700

    Changes

---
+++ Assets/Scripts/Jason/Fan.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fan : MonoBehaviour {
+	public float force = 10;
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Player")
+		{	
+			Debug.Log ("Blowing");
+			other.rigidbody2D.AddRelativeForce(gameObject.transform.up*force, ForceMode2D.Impulse);
+		}
+	}
+}
