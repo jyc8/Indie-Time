7934a61bb283b1c00abe3bc54c72a1643b479afc
Jason <jyc8@live.com>
2014-07-30 15:40:10 -0700

    Level 3 Basic Layout Done

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator : MonoBehaviour {
+	public GameObject Target;
+	private bool active = true;
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Player" && active)
+		{	
+			Debug.Log ("Teleporting");
+			Target.GetComponent<Elevator>().Deactivate();
+			other.rigidbody2D.velocity = Vector2.zero;
+			other.transform.position = Target.transform.position;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		active = true;
+	}
+
+	public void Deactivate(){
+		active = false;
+	}
+}
