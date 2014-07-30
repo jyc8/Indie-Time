8ef1e569e0812c24d4e3261fc2eb41014b3a1a5e
Jason <jyc8@live.com>
2014-07-30 00:31:58 -0700

    Stuff

--- Assets/Scripts/Jason/PlanetSpin.cs
+++ Assets/Scripts/Jason/PlanetSpin.cs
@@ -5,7 +5,37 @@ public class PlanetSpin : MonoBehaviour {
 	public GameObject planet;
 
 	// Update is called once per frame
-	void Update () {
+	void Start() {
+		TriggerAllEvents();
+	}
 
+	void FixedUpdate() {
+		// Cache the horizontal input.
+		float h = Input.GetAxis("Horizontal");
+		
+		// If the input is moving the player right and the player is facing left...
+		if(h > 0){
+			Debug.Log("Moving Right");
+		}else if(h < 0){
+			Debug.Log("Moving Left");
+		}
+			
+	}
+
+
+	void PopChildren(Transform TriggerObject, bool Bool){
+		//Debug.Log(TriggerObject.childCount);
+		if (TriggerObject.childCount > 0){
+			for (int i = 0; i < TriggerObject.childCount; i++){
+				if (TriggerObject.GetChild(i).GetComponent<StateSelector>() != null){
+					TriggerObject.GetChild(i).GetComponent<StateSelector>().Pop(Bool);
+				}
+				PopChildren(TriggerObject.GetChild(i), Bool);
+			}
+		}
+	}
+	
+	void TriggerAllEvents(){
+		PopChildren(transform, true);
 	}
 }
