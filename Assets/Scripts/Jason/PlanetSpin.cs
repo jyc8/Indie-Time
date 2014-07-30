e743d88faf91bfe4874a272aaa00a3ecac172260
Jason <jyc8@live.com>
2014-07-30 00:51:58 -0700

    The planet spins shit

--- Assets/Scripts/Jason/PlanetSpin.cs
+++ Assets/Scripts/Jason/PlanetSpin.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PlanetSpin : MonoBehaviour {
 	public GameObject planet;
+	public float speed = 0.5f;
 
 	// Update is called once per frame
 	void Start() {
@@ -16,10 +17,11 @@ public class PlanetSpin : MonoBehaviour {
 		// If the input is moving the player right and the player is facing left...
 		if(h > 0){
 			Debug.Log("Moving Right");
+			transform.Rotate(Vector3.forward * speed);
 		}else if(h < 0){
 			Debug.Log("Moving Left");
-		}
-			
+			transform.Rotate(Vector3.back * speed);
+		}			
 	}
 
 
