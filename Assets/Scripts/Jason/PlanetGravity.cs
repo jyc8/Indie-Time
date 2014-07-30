7177e47d09b61d32bfee6a63af65d3b774eaa905
Jason <jyc8@live.com>
2014-07-29 23:59:07 -0700

    Planet Gravity

---
+++ Assets/Scripts/Jason/PlanetGravity.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetGravity : MonoBehaviour {
+	public float gravStrength = 10;
+	public GameObject planet;
+
+	// Update is called once per frame
+	void Update () {
+		Vector2 planetVector = planet.transform.position - transform.position;
+		rigidbody2D.AddForce(planetVector.normalized * gravStrength);
+	}
+}
