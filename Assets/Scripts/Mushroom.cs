4636faa29fd1170881796d1f43bd43e0d52cf5c4
Jason <jyc8@live.com>
2014-07-30 17:17:43 -0700

    Mushroom

--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -2,13 +2,13 @@
 using System.Collections;
 
 public class Mushroom : MonoBehaviour {
-	public float bounceForce = 10;
-	
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.gameObject.tag == "Player")
 		{	
 			Debug.Log ("Bounce");
-			other.rigidbody2D.AddForce(bounceForce * other.transform.up, ForceMode2D.Impulse);
+			//other.rigidbody2D.AddForce(bounceForce * other.transform.up, ForceMode2D.Impulse);
+			other.rigidbody2D.velocity = Vector2.zero;
+			other.rigidbody2D.AddForce(new Vector2(0f, 1500));
 		}
 	}
 }
