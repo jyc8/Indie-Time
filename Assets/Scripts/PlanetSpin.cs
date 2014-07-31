7c2010870ed99f47fc59e26cd498bd9e454dec4b
Jason <jyc8@live.com>
2014-07-30 20:00:47 -0700

    ADS

--- Assets/Scripts/PlanetSpin.cs
+++ Assets/Scripts/PlanetSpin.cs
@@ -5,21 +5,19 @@ public class PlanetSpin : MonoBehaviour {
 	public GameObject planet;
 	public GameObject musicMachine;
 	public float speed = 0.5f;
-
-	// Update is called once per frame
-	void Start() {
-
-	}
+	private bool triggered = false;
 
 	void Update() {
 		// Cache the horizontal input.
 		float h = Input.GetAxis("Horizontal");
-		
+		if (triggered == false){
+			musicMachine.GetComponent<MusicMachine>().TriggerAll();
+			triggered = true;
+		}
 		// If the input is moving the player right and the player is facing left...
 		if(h > 0){
 			//Debug.Log("Moving Right");
 			transform.Rotate(Vector3.forward * speed);
-			musicMachine.GetComponent<MusicMachine>().TriggerAll();
 		}else if(h < 0){
 			//Debug.Log("Moving Left");
 			transform.Rotate(Vector3.back * speed);
