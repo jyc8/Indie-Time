72c7ea65af77140be63452f1265737aca6d79b40
Jason <jyc8@live.com>
2014-08-07 14:13:22 -0700

    Fan Fixed

--- Assets/Scripts/Fan.cs
+++ Assets/Scripts/Fan.cs
@@ -2,15 +2,13 @@
 using System.Collections;
 
 public class Fan : MonoBehaviour {
-	public float force = 10;
-	
-	void OnTriggerEnter2D(Collider2D other){
+	void OnTriggerStay2D(Collider2D other){
 		if (other.gameObject.tag == "Player")
 		{	
 			Debug.Log ("Blowing");
 			//other.rigidbody2D.AddRelativeForce(gameObject.transform.up*force, ForceMode2D.Impulse);
 			other.rigidbody2D.velocity = Vector2.zero;
-			other.rigidbody2D.AddForce(new Vector2(0f, 800));
+			other.rigidbody2D.AddForce(new Vector2(0f, 250));
 		}
 	}
 }
